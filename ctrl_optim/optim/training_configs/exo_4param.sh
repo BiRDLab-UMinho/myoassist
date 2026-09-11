3699011bddffc82d656b4ec05a30b5419368142c
saramonteiro <asaramonterio49@gmail.com>
2026-09-11 15:19:58 +0100

    Added exo_4param.sh CMA_ES configuration file

---
+++ ctrl_optim/optim/training_configs/exo_4param.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+exec "$PYTHON_CMD" -m ctrl_optim.optim.train \
+    --msk myolegs22 \
+    --device Humotech_L1 \
+    --sim_time 20 \
+    --pose_key walk_left \
+    --num_strides 5 \
+    --delayed 0 \
+    --optim_mode single \
+    --reflex_mode uni \
+    --tgt_vel 1.0 \
+    --trunk_err_type ref_diff \
+    --tgt_sym_th 0.1 \
+    --tgt_grf_th 1.5 \
+    -kine \
+    --ExoOn 1 \
+    --use_4param_spline \
+    --max_torque 10.0 \
+    --popsize 32 \
+    --maxiter 1000 \
+    --threads 8 \
+    --sigma_gain 10 \
+    --save_path exo_4param
